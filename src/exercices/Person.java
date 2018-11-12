35be711c268f630fb70eec93021249f8f5afdf52
Véronique Sermage <ransomstoddart@gmail.com>
2018-11-12 13:03:00 +0100

    Nouveau fichier Person.java

--- src/exercices/Person.java
+++ src/exercices/Person.java
@@ -6,35 +6,55 @@ public class Person {
     // - name (String)
     // - age (int)
     // - wilder (boolean)
-
+	 private String name;
+	 private int age;
+	 private boolean wilder;
 
     // TODO 2. Add a Constructor with name and age arguments (in this order)
     // This constructor must initialize name and age attributes
-
+	public Person (String name, int age){
+		this.name=name;
+		this.age=age;
+	}
 
     // TODO 3. Add getter method for name attribute
-
+	public String getName(){
+		return name;
+	}
 
     // TODO 4. Add setter method for name attribute
-
+	public void setName(String name){
+		this.name=name;
+	}
 
     // TODO 5. Add getter method for age attribute
 
+	public int getAge(){
+		return age;
+	}
 
     // TODO 6. Add setter method for age attribute
-
+	public void setAge(int age){
+		this.age=age;
+	}
 
     // TODO 7. Add getter method for wilder attribute
-
+	public boolean isWilder(){
+		return wilder;
+	}
 
     // TODO 8. Add setter method for wilder attribute
+	public void setWilder(boolean wilder){
+		this.wilder=wilder;
+	}
 
-
+    // where you replace {name} and {age} with attributes values		
     // TODO 9. Create a method 'whoAmI' that returns "My name is {name} and I'm {age}"
-    // where you replace {name} and {age} with attributes values
-
-
-
+	public String whoAmI(){
+		String p=getName();
+		int i=getAge();
+		return "My name is " +p + " and I'm " + i;
+	}
 
     // Do not remove this empty constructor !
     public Person() {
